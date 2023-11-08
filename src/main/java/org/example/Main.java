00acd321c82aaed998fc21d4051c29a2b5128abc
sriramun <sriramun2@gmail.com>
2023-11-08 16:19:24 +0530

    Added sample tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,82 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static double add(double a, double b) {
+        logger.info("Attempt Operation Addition");
+        double res = a+b;
+        logger.info("Attempt Successful!");
+        
+        return res;
+    }
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        logger.info("Start of execution");
+        System.out.printf("Hello and welcome to Calculator v0.0.0!\n");
+        
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        while(true) {
+            System.out.println("Pick an operation:");
+            System.out.println("0: Addition");
+            System.out.println("1: Subtraction");
+            System.out.println("2: Multiplication");
+            System.out.println("3: Division");
+            System.out.println("4: Exit the program");
+            System.out.print("Enter your choice (0-4): ");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            int operation = scanner.nextInt();
+
+            if(operation == 4) {
+                logger.info("End of execution");
+                System.out.println("Exitting Calculator");
+                break;
+            }
+
+            System.out.print("Enter the first number: ");
+            double firstNumber = scanner.nextDouble();
+
+            System.out.print("Enter the second number: ");
+            double secondNumber = scanner.nextDouble();
+
+            double result = -1;
+
+            switch (operation) {
+                case 0: // Addition
+                    result = add(firstNumber, secondNumber);
+                    break;
+                // case 1: // Subtraction
+                //     result = subtract(firstNumber, secondNumber);
+                //     break;
+                // case 2: // Multiplication
+                //     result = multiply(firstNumber, secondNumber);
+                //     break;
+                // case 3: // Division
+                //     // Check for division by zero
+                //     if (secondNumber == 0) {
+                //         System.out.println("Cannot divide by zero.");
+                //         continue;
+                //     }
+                //     result = divide(firstNumber, secondNumber);
+                //     break;
+                default:
+                    logger.warn("Invalid Input");
+                    System.out.println("Invalid operation. Please try again.");
+                    continue;
+            }
+
+            System.out.printf("The result is: %.2f%n", result);
+            System.out.println();
         }
+
+        scanner.close();
     }
 }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import org.example.Main;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Main calculator;
+
+    @Before
+    public void setup() {
+        calculator = new Main();
+    }
+
+    @Test
+    public void test_add_positive() {
+        double a = 1;
+        double b = 2;
+        double expectedResult = 3;
+        Assert.assertEquals(expectedResult, calculator.add(a, b), 0.001);
+    }
+    
+    @Test
+    public void test_add_negative() {
+        double a = 1;
+        double b = 2;
+        double expectedResult = 0;
+        Assert.assertNotEquals(expectedResult, calculator.add(a, b), 0.001);
+    }
+}
